02afd1ffd71114786ff4fe76299cfad31fa54419
Andre <andredelwyn@gmail.com>
2017-01-12 23:54:53 +0200

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var router = express.Router();
+
+var appdata = require('../data.json');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    var myArtwork = [];
+    var myArtists =[];
+    myArtists = appdata.speakers;
+    
+    appdata.speakers.forEach(function(item){
+       myArtwork = myArtwork.concat(item.artwork) 
+    });
+    res.render('index', { 
+      title: 'Home', 
+      artwork: myArtwork,
+      artists: myArtists,
+      page: 'home'
+  });
+});
+
+module.exports = router;
+
+router.get('/speakers', function(req, res){
+    var myArtwork = [];
+    var myArtists = [];
+    myArtists = appdata.speakers;
+    
+    appdata.speakers.forEach(function(item){
+       myArtwork = myArtwork.concat(item.artwork);
+    });
+   res.render('speakers', {
+       title: 'Speakers', 
+       artwork: myArtwork,
+       artists: myArtists,
+       page: 'artistList'
+   }); 
+});
+
+router.get('/speakers/:speakerid', function(req, res){
+    var myArtwork = [];
+    var myArtists = [];
+
+    appdata.speakers.forEach(function(item){
+        if(item.shortname == req.params.speakerid){
+           myArtwork = myArtwork.concat(item.artwork);   
+           myArtists.push(item); 
+        }
+    });
+   res.render('speakers', {
+       title: 'Speakers', 
+       artwork: myArtwork,
+       artists: myArtists,
+       page: 'artistDetail'
+       
+   }); 
+});
